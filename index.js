d705d4555da76778deed53ec3cb9d7fc6415e84d
Matt Gibbs <matt.gibbs@hp.com>
2015-09-20 22:16:54 -0400

    Snapshot of generation.
    About to parse the css back into some javascript variables.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var path = require('path');
 var accord = require('accord');
 var gutil = require('gulp-util');
 var through2 = require('through2');
+var css = require('css');
 
 var PluginError = gutil.PluginError;
 var less = accord.load('less');
@@ -18,27 +19,37 @@ module.exports = function (options) {
             return callback(new PluginError('gulp-less-branding-js', 'Streaming not supported.'));
         }
 
-        // regex for pulling out variables to make holder classes for them
-        // curse you javascript for not supporting look-behind!
-        // (?<=@)([A-Za-z0-9\-\_]+)(?=:)
-
         var variableRegex = /(@[A-Za-z0-9\-_]+)(?=:)/g;
-
         var contentsStr = file.contents.toString();
-
-        var variables = contentsStr.match(variableRegex);
-
-        console.log(variables.length);
-
-        variables.forEach(function (variable) {
-            variable = variable.slice(1);
-            var clazz = ['.', variable, ' { color: @', variable, '; }'].join('');
-            console.log(clazz);
-            contentsStr = contentsStr.concat(clazz);
+        var variables = contentsStr.match(variableRegex).map(function (obj) {
+            return obj.slice(1);
         });
 
 
-        return file;
+        variables.forEach(function (variable) {
+            var clazz = ['.', variable, ' { color: @', variable, '; }\r\n'].join('');
+            contentsStr = contentsStr.concat(clazz);
+        });
+
+        console.log(contentsStr);
+
+        var opts = {filename: file.path, compress: false};
+        less.render(contentsStr, opts).then(function (res) {
+
+            var compiledCss = new Buffer(res.result);
+            var ast = css.parse(compiledCss);
+
+
+            file.contents = new Buffer(res.result);
+
+
+            return file;
+        }).then(function (file) {
+            callback(null, file);
+        }).catch(function (err) {
+            console.log(err);
+            throw new PluginError('gulp-less-branding-js', err);
+        }).done(undefined, callback);
     });
 
 }
