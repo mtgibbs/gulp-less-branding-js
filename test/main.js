4d9c2f975be6febed97b338aed0a6a9a84d26a3d
Matt Gibbs <matt.gibbs@hp.com>
2015-09-20 21:04:47 -0400

    Skeleton of unit tests.

---
+++ test/main.js
@@ -0,0 +1,26 @@
+var should = require('should');
+var less = require('../');
+var gutil = require('gulp-util');
+var fs = require('fs');
+var path = require('path');
+
+function createVinyl(brandingFileName, contents) {
+    var base = path.join(__dirname, 'fixtures');
+    var filePath = path.join(base, brandingFileName);
+
+    return new gutil.File({
+        cwd: __dirname,
+        base: base,
+        path: filePath,
+        contents: contents || fs.readFileSync(filePath)
+    });
+}
+
+describe('gulp-less-branding-js', function () {
+    describe('brandingToJS()', function () {
+
+        it('should compile a _branding.less to _branding.js', function (done) {
+           var brandingFile = createVinyl('')
+        });
+    });
+});
