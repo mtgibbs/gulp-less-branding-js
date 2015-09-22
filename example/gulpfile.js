db5c894da245f69f38377c091fa83b0b38c9c502
Matt Gibbs <matt.gibbs@hp.com>
2015-09-22 17:57:41 -0400

    Adding to examples

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -1,10 +1,22 @@
 var gulp = require('gulp');
 var brandingToJS = require('../');
 
-gulp.task('brandingToJS', function(){
-  gulp.src('./_branding.less')
-  .pipe(brandingToJS())
-  .pipe(gulp.dest('build'));
+gulp.task('brandingToJS', function () {
+    gulp.src('./_branding.less')
+        .pipe(brandingToJS())
+        .pipe(gulp.dest('build'));
 });
 
-gulp.task('default', ['brandingToJS']);
+gulp.task('brandingToTS', function () {
+    gulp.src('./_branding.less')
+        .pipe(brandingToJS({format: 'ts'}))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('brandingToCoffee', function() {
+    gulp.src('./_branding.less')
+        .pipe(brandingToJS({format: 'coffee'}))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('default', ['brandingToJS', 'brandingToTS', 'brandingToCoffee']);
